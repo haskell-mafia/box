1bb5b5ba729199c764df1f8b6de8f065dbdb31fb
Jacob Stanley <jacob@stanley.io>
2015-08-27 10:09:35 +1000

    Much slicker zsh completion

--- _box.zsh
+++ _box.zsh
@@ -8,4 +8,15 @@ for arg in ${words[@]}; do
   request=(${request[@]} --bash-completion-word $arg)
 done
 
-compadd -- $( box "${request[@]}" )
+completions=($( box "${request[@]}" ))
+
+for word in $completions; do
+  # for words ending in a colon (:) treat the
+  # colon as a suffix which is overwritten when
+  # the user types a character.
+  if [[ $word[-1] == ":" ]]; then
+    compadd -S \: -q -- $word[0,-2]
+  else
+    compadd -- $word
+  fi
+done
