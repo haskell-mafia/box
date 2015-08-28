16345623d314eb28db67fb494f648c75e8c3adff
Jacob Stanley <jacob@stanley.io>
2015-08-28 10:01:22 +1000

    fixup! Even fancier zsh completion

--- _box.zsh
+++ _box.zsh
@@ -25,13 +25,23 @@ else
     parts=(${(s,:,)word})
     lines=($parts[-1])
 
-    # for words ending in a colon (:) treat the
-    # colon as a suffix which is overwritten when
-    # the user types a character.
     if [[ $word[-1] == ":" ]]; then
+      # for words ending in a colon (:) treat the
+      # colon as a suffix which is overwritten when
+      # the user types a character
       compadd -S \: -q -l -d lines -- $word[0,-2]
+
+    elif [[ $word[1] == "-" ]]; then
+      # words starting with a dash (-) are
+      # flags, so put them at the bottom
+      # instead of in the list     
+      compadd                      -- $word
+
     else
+      # everything else goes in the list
+      # but had no special handling for colon
       compadd          -l -d lines -- $word
+
     fi
   done
 fi
