3bb0657aaa9c3c8a566ce7297ce239f1380826a6
Jacob Stanley <jacob@stanley.io>
2015-09-17 13:46:48 +1000

    Removed unused imports

--- test/Test/IO/Box/Store.hs
+++ test/Test/IO/Box/Store.hs
@@ -7,19 +7,11 @@ module Test.IO.Box.Store where
 import           Box.Data
 import           Box.Store
 
-import           Control.Monad.Trans.Either
-
-import qualified Data.Text as T
-
 import           P
 
-import           System.IO (IO)
-
 import           Test.Box.Arbitrary ()
-import           Test.Mismi.S3
 import           Test.Mismi.Amazonka
 import           Test.QuickCheck
-import           Test.QuickCheck.Property (failed)
 
 
 prop_readwrite_s3 bs = withAWS (writeReadBoxes bs . BoxStoreS3)
